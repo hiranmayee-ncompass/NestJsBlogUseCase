c3cfd4682d8ecd500fc12ce09ee5c60e02bdb6ab
Hiranmayee Mishra <hiranmayee@ncompass.inc>
2024-02-15 15:14:56 +0530

    Implemented JWT Authorisation

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+// auth/jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { UserService } from '../../users/services/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'hmkey',
+    });
+  }
+
+  validate(payload: any): any {
+    return payload;
+  }
+}
